671be3b0a9612c663ac4f5e4e857493fd95f4311
Bankimchandrayadav <bankim.97@gmail.com>
2022-05-10 14:59:35 +0530

    Create 01_daily_to_netcdf_2000_2009.py

---
+++ Selected_Bonus_Modules_1_Data_Packaging/01_daily_to_netcdf_2000_2009.py
@@ -0,0 +1,70 @@
+# %% [markdown]
+# # About 
+# This code is for the conversion of modelled daily tiff files to netcdf format for the bkgn period i.e. 2000-2009
+
+
+
+
+# %% [markdown]
+## Libs and local vars
+from tqdm.notebook import tqdm as td 
+from datetime import datetime
+import glob, rioxarray as rx, shutil, xarray as xr, numpy as np, time, os, gdal
+start = time.time()
+def fresh(where):
+    if os.path.exists(where):
+        shutil.rmtree(where)
+        os.mkdir(where)
+    else:
+        os.mkdir(where)  
+
+
+
+
+# %% [markdown]
+## Convert tiff files to netcdf [1972-2018]
+# [1] read and subset files to bkgn duration
+rasters = sorted(glob.glob("../../3_RSData/1_Rasters/GeneratedRasters/01_Daily/01_2000_2018_GPM/*.tif"))
+dateEnd = '2009_12_31'  # subset data fill 2009 Dec 31
+for i in range(len(rasters)):  # find the index of 20091231 file 
+    if dateEnd in rasters[i]:
+        print(rasters[i], 'at index', i)  # index of 20091231 file found 
+rasters = rasters[:3501]  # subset done
+
+# [2] set out directory
+outDir = "../../3_RSData/1_Rasters/GeneratedRasters/02_DailyNetcdf/04_2000_2009_GPM/"
+fresh(outDir)  
+
+#[3.1] convert using rioxarray 
+for i in td(range(len(rasters)), desc='Converting'):  
+    date = rasters[i].split('/')[-1].split('.')[0].replace("_","-")  # date extracted
+    outName = outDir + date + '.nc'  # outfile name set
+    da = rx.open_rasterio(rasters[i])  # read as a dataarray
+    da = da.rename('prcp_renoj')  # variable given name 
+
+    # [3.2] set year, month and day variables
+    year = int(date.split('-')[0])  
+    month = int(date.split('-')[1])
+    day = int(date.split('-')[2])
+
+    # [3.3] reopen the saved nc file and change some properties
+    da = da.assign_coords(time=datetime(year, month, day))  # time assigned
+    da = da.expand_dims('time')  # time added to dimesion (was optional)
+    da.to_netcdf(outName, engine="h5netcdf", invalid_netcdf=True)  
+
+
+
+
+# %% [markdown]
+## Convert multiple netcdf to single netcdf 
+# [1.1] set paths
+rasMultiple = sorted(glob.glob(outDir + "*.nc"))
+outDir1 = outDir + "SingleFile/"
+fresh(outDir1)  
+
+# [2] conversion
+ds = xr.open_mfdataset(rasMultiple, concat_dim='time')
+outName = outDir1 + "prcp_2000_2009.nc"  # outfile name set 
+ds.to_netcdf(outName)  # saved to a single netcdf
+print('Files saved into a single netcdf')
+print('Time elapsed: {} secs'.format(np.round((time.time()-start), 2)))
